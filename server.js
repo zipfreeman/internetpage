89f5a3918e9b93f6ba20adcd868a5b81c41fb220
Zach Rodis <zach@Zachs-MacBook-Air.local>
2018-10-31 16:32:28 -0400

    added dynamic port, start script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var dir = __dirname + '/public'
 var app = express();
@@ -75,6 +76,6 @@ app.get('/bad', (req, res) => {
 
 
 
-app.listen('3000', () => {
-  console.log('server do')
+app.listen(port, () => {
+  console.log('server do '+ port)
 });
